d798e8501ee4e220c457dd5760629ff501973f32
rrathje <rilyr1997@gmail.com>
2020-09-18 21:43:39 -0400

    Everything now works

--- public/js/profile.app.js
+++ public/js/profile.app.js
@@ -20,13 +20,15 @@ var app = new Vue({
           var userData = data.results[0];
           console.log(userData);
           this.userName = userData.name.first + " " + userData.name.last;
-          this.userDOB = userData.dob[0];
-          this.userAge = userData.dob[1];
+          this.userDOB = userData.dob.date;
+          this.userAge = userData.dob.age;
           this.userEmail = userData.email;
           this.userCountry = userData.location.country;
           this.userImgLrg = userData.picture.large;
           this.userImgThb = userData.picture.thumbnail;
         });
+      },
+        formatDate(d) {return moment(d).format("MMM Do YYYY");
       }
-    }
+    },
   })
